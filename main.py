ab3f5e0161a554e4a7f5847e4981007248f7b646
musasyed <musasyed16@gmail.com>
2023-06-12 11:28:22 +0000

    This is updated  Main File

---
+++ main.py
@@ -0,0 +1,8 @@
+from add import add
+from subtract import subtract
+
+# Taking Input
+a=int(input('Enter 1st Number: '))
+b=int(input('Enter 2nd Number: '))
+result=add(a,b)
+print('The Sum is: ' , result)
